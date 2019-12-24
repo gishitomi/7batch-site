5256634c2cab95c69c5e015562d3519b8d593a7a
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-25 00:21:55 +0900

    よろしくお願いいたします

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>7batch</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <div id="index">
+
+
+    
+    </div>
+</body>
+</html>
