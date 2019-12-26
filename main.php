c9291c525851f9e591a245a42f4e6b1c6a5a227c
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-26 09:58:32 +0900

    イメージがわかない笑

--- main.php
+++ main.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>7batchサイト</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <!-- <link rel="stylesheet" href="./assets/css/style.css"> -->
+    <link rel="stylesheet" href="./assets/css/main.css">
 </head>
 
 <body>
     <div id="main">
         <header>
-            <p>7batch</p>
+            <h1>7batch</h1>
             <div class="header-img">
                 <img src="./assets/img/IMG_7444.jpeg" alt="">
             </div>
